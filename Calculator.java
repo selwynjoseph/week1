3ee23587177e7a967a352de05079101f98db48ad
selwynjoseph <91867939+selwynjoseph@users.noreply.github.com>
2021-10-04 17:49:45 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,61 @@
+package week1.day2;
+
+public class Calculator {
+
+	/*int num1 = 20;
+	int num2 = 12;
+	int num3 = 20;
+	int result;*/
+	
+	//addition
+	public int add()
+	{
+		int num1=20;
+		int num2=12;
+		int num3=30;
+		int result=num1+num2+num3;
+		return result;
+		
+	}
+	
+	//subtraction
+	public int subtract()
+	{
+		int num1=20;
+		int num2=12;
+		int result=num1-num2;
+		return result;
+		
+	}
+	
+	//multiplication
+	public double multiplication()
+	{
+		double num1=20;
+		double num2=12;
+		double result=num1*num2;
+		return result;
+		
+	}
+	
+	//division
+	public float division()
+	{
+		float num1=20;
+		float num2=12;
+		float result=num1/num2;
+		return result;
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		
+				
+		
+	}
+
+
+
+
+}
---
+++ MyCalculator.java
@@ -0,0 +1,18 @@
+package week1.day2;
+
+public class MyCalculator {
+
+	public static void main(String[] args) {
+		Calculator obj2=new Calculator();
+		
+		System.out.println("Addition value is " + obj2.add());
+		System.out.println("Subtraction value is " + obj2.subtract());
+		System.out.println("Multiplication value is " + obj2.multiplication());
+		System.out.println("Division value is " + obj2.division());
+		
+		
+		
+
+	}
+
+}

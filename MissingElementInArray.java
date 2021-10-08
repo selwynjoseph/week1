e678e624a6060da805b0dcb489841f10641cbb07
selwynjoseph <91867939+selwynjoseph@users.noreply.github.com>
2021-10-08 12:06:41 +0530

    Update MissingElementInArray.java

--- MissingElementInArray.java
+++ MissingElementInArray.java
@@ -6,7 +6,7 @@ public class MissingElementInArray {
 
 	public static void main(String[] args) 
 	{
-		//initialze the array
+		//initialize the array
 		int[] array= {1,2,3,4,7,6,8};
 		
 		
@@ -16,11 +16,28 @@ public class MissingElementInArray {
 		//sort the array
 		Arrays.sort(array);
 		System.out.println("Sorted array is ");
+		
 		for(int i=0; i<length; i++)
 		{
-			System.out.println(array[i]);
+			
 		}
 		
+		//print the sorted array as a string
+		System.out.println(Arrays.toString(array));
+		
+		for(int i=0; i<length; i++)
+		{
+			
+			if (array[i]+1 != array[i+1]) 
+			{
+				// print the number
+				System.out.println("Missing element in the array:" + (array[i]+1));
+				// once printed break the iteration
+				break;
+			}
+		
+		
+		}
 
 	}
 
